01acdcd49c46b5d2313c96573335ed122afe0d5d
Piotr Olchawa <piotrusolchawa@gmail.com>
2010-11-24 06:04:51 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Capoeira::Application.config.secret_token = 'a85ffc1b610ca19507817e7924716faedaafe2b790a7f85af5e406be2c824a715f52d759b930d6a04bbb4ebeb627955b32cb1e360f8492a0eb4646737a3b2095'
