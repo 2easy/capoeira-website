96825cf0b3eab2a53ae051b0daad34d774dfae6e
Piotr Olchawa <piotrusolchawa@gmail.com>
2010-12-13 23:19:11 +0100

    simple layout added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Capoeira::Application.routes.draw do
   devise_for :users , :controllers => {:omniauth_callbacks => "users/omniauth_callbacks" }
 
+  resources :news
+
   root :to => "home#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
